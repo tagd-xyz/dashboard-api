8579581727e7af513b15541f03a6d3449c9afb75
Juan Sierra <juan@totally.group>
2023-06-05 22:21:49 +0200

    TAGD-56 - Add reporting for retailers

---
+++ app/Http/V1/Controllers/Retailers/Reporting/Ref.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\V1\Controllers\Retailers\Reporting;
+
+use App\Http\V1\Controllers\Controller as Controller;
+use Illuminate\Http\Request;
+use Tagd\Core\Models\Item\Item as ItemModel;
+
+class Ref extends Controller
+{
+    protected function brands(Request $request)
+    {
+        $actingAs = $this->actingAs($request);
+
+        $brands = ItemModel::query()
+            ->where('retailer_id', $actingAs->id)
+            ->whereNotNull('properties->brand')
+            ->select('properties->brand as name')
+            ->distinct('name')
+            ->get();
+
+        return response()->withData(
+            $brands
+        );
+    }
+}

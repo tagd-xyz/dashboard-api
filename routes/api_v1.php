723f36faa37212ed0eeb8ac71751359ab45d49e2
Juan Sierra <juan@totally.group>
2023-06-11 22:23:07 +0200

    TAGD-56 - Add reporting for resellers

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -28,6 +28,30 @@ Route::namespace('\App\Http\V1\Controllers')->group(function () {
                         Route::get('list', 'TagsIssued@index');
                         Route::get('graph', 'TagsIssued@graph');
                     });
+
+                    Route::prefix('time-to-transfer')
+                    ->group(function () {
+                        Route::get('list', 'TimeToTransfer@index');
+                        Route::get('graph', 'TimeToTransfer@graph');
+                    });
+            });
+        });
+
+        Route::prefix('resellers')
+            ->group(function () {
+            Route::prefix('reporting')
+                ->namespace('\App\Http\V1\Controllers\Resellers\Reporting')
+                ->group(function () {
+                    Route::prefix('ref')
+                    ->group(function () {
+                        Route::get('brands', 'Ref@brands');
+                    });
+
+                    Route::prefix('time-to-transfer')
+                    ->group(function () {
+                        Route::get('list', 'TimeToTransfer@index');
+                        Route::get('graph', 'TimeToTransfer@graph');
+                    });
             });
         });
 

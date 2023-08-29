678ce521017738f4a499a22ddc528dc182280e9b
Juan Sierra <juan@totally.group>
2023-08-29 22:41:41 +0200

    Update laravel/pint and fix code style

--- app/Http/V1/Controllers/Retailers/Reporting/PopularTypes.php
+++ app/Http/V1/Controllers/Retailers/Reporting/PopularTypes.php
@@ -19,20 +19,20 @@ class PopularTypes extends Controller
         $until = Carbon::now();
 
         $data = TagdModel::join('items', 'items.id', '=', 'tagds.item_id')
-                ->join('item_types', 'item_types.id', '=', 'items.type_id')
-                ->whereNull('tagds.parent_id')
-                ->whereHas('item', function (Builder $query) use ($actingAs) {
-                    $query->where('retailer_id', $actingAs->id);
-                })
-                ->whereHas('children', function (Builder $query) {
-                    $query->where('status', TagdStatus::TRANSFERRED);
-                })
-                ->whereBetween('tagds.created_at', [$since, $until])
-                ->selectRaw('item_types.id, item_types.name, count(*) as total')
-                ->groupBy('items.type_id')
-                ->orderBy('total', 'desc')
-                ->limit(5)
-                ->get();
+            ->join('item_types', 'item_types.id', '=', 'items.type_id')
+            ->whereNull('tagds.parent_id')
+            ->whereHas('item', function (Builder $query) use ($actingAs) {
+                $query->where('retailer_id', $actingAs->id);
+            })
+            ->whereHas('children', function (Builder $query) {
+                $query->where('status', TagdStatus::TRANSFERRED);
+            })
+            ->whereBetween('tagds.created_at', [$since, $until])
+            ->selectRaw('item_types.id, item_types.name, count(*) as total')
+            ->groupBy('items.type_id')
+            ->orderBy('total', 'desc')
+            ->limit(5)
+            ->get();
 
         return response()->withData(
             $data
